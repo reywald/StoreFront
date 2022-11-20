b46d76fbbfd3e23a77c00b329332a2b929007139
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-11-20 22:50:21 +0100

    Resolving Circular Relationships

--- store/models.py
+++ store/models.py
@@ -8,6 +8,8 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        "Product", on_delete=models.SET_NULL, null=True, related_name="+")
 
 
 class Product(models.Model):
@@ -26,9 +28,9 @@ class Customer(models.Model):
     MEMBERSHIP_SILVER = 'S'
     MEMBERSHIP_GOLD = 'G'
     MEMBERSHIP_STATUS = [
-        MEMBERSHIP_BRONZE, 'Bronze',
-        MEMBERSHIP_SILVER, 'Silver',
-        MEMBERSHIP_GOLD, 'Gold'
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold')
     ]
 
     first_name = models.CharField(max_length=255)
@@ -41,26 +43,20 @@ class Customer(models.Model):
 
 
 class Order(models.Model):
-    PAYMENT_PENDING = 'Pending'
-    PAYMENT_COMPLETE = 'Complete'
-    PAYMENT_FAILED = 'Failed'
+
+    PAYMENT_PENDING = 'P'
+    PAYMENT_COMPLETE = 'C'
+    PAYMENT_FAILED = 'F'
     PAYMENT_STATUS_CHOICES = [
-        (PAYMENT_PENDING, 'P'),
-        (PAYMENT_COMPLETE, 'C'),
-        (PAYMENT_FAILED, 'F')
+        (PAYMENT_PENDING, 'Pending'),
+        (PAYMENT_COMPLETE, 'Complete'),
+        (PAYMENT_FAILED, 'Failed')
     ]
 
     placed_at = models.DateTimeField(auto_now_add=True)
-    payment_status = models.CharField(
-        choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_PENDING)
+    payment_status = models.CharField(max_length=1,
+                                      choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
-
-
-class Address(models.Model):
-    street = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    customer = models.ForeignKey(
-        Customer, on_delete=models.CASCADE)
 
 
 class OrderItem(models.Model):
@@ -68,6 +64,12 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
 
 class Cart(models.Model):
