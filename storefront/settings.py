e24a9e728ac6f92a30aad381511bb457b2a8f7e6
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-11-24 06:52:51 +0100

    Using MySQL in Django

--- storefront/settings.py
+++ storefront/settings.py
@@ -88,8 +88,10 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'USER': 'root',
+        'PASSWORD': 'RS,Ykm$7oAMx=xED2~`T'
     }
 }
 
