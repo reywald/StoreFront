3eda52a1da60af2619b497db105192ac244394ec
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-11-29 16:36:05 +0100

    Complex Lookups Using Q Objects

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.db.models import Q
 from store.models import Product
 
 
 def say_hello(request):
-    products = Product.objects.filter(collection__title="Stationary")
+    products = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20))
 
     return render(request, "hello.html", {"name": "Ikechukwu Agbarakwe", "products": list(products)})
