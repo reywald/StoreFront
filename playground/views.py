ee23578675dc43e2cc928c6db41946af8fec2003
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-11-29 12:41:49 +0100

    Filtering Objects

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,8 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def say_hello(request):
-    # Method 1: Using exceptions to catch objects not existing
-    try:
-        product = Product.objects.get(pk=0)
-    except ObjectDoesNotExist:
-        pass
+    products = Product.objects.filter(collection__title="Stationary")
 
-    # Method 2: Using filters to detect objects' existence
-    exists = Product.objects.filter(pk=0).exists()
-    product = Product.objects.filter(pk=0).first()
-
-    return render(request, "hello.html", {"name": "Ikechukwu Agbarakwe"})
+    return render(request, "hello.html", {"name": "Ikechukwu Agbarakwe", "products": list(products)})
