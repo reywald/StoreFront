5b6748d3beb5ae329a9a7fac54739e050e31df06
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-12-03 22:32:50 +0100

    Aggregating Objects

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from django.db.models import F
-# from store.models import Product
-from store.models import Order
-from store.models import OrderItem
+from django.db.models.aggregates import Count, Min
+from store.models import Product
+
 
 
 def say_hello(request):
-    queryset = Order.objects.select_related("customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
-
-    return render(request, "hello.html", {"name": "Ikechukwu Agbarakwe", "orders": queryset})
+    result = Product.objects.aggregate(count=Count("id"), min=Min("unit_price"))
+    return render(request, "hello.html", {"name": "Ikechukwu Agbarakwe", "result": result})
