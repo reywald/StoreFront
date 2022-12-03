ffbb5296a1e0a3c4087eddc09705a9b060b47bbd
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-12-03 17:16:59 +0100

    Selecting Fields to Query

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.db.models import Q, F
+from django.db.models import F
 from store.models import Product
 
 
 def say_hello(request):
-    products = Product.objects.filter(inventory=F('collection__id'))
+    queryset = Product.objects.filter(
+        orderitem__product=F("pk")).distinct().order_by("title")
 
-    return render(request, "hello.html", {"name": "Ikechukwu Agbarakwe", "products": list(products)})
+    return render(request, "hello.html", {"name": "Ikechukwu Agbarakwe", "products": queryset})
