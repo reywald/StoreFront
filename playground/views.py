79a9f20c0a909f55599584b2fb1e039597758cc8
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-12-03 22:14:13 +0100

    Selecting Related Objects

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.db.models import F
-from store.models import Product
+# from store.models import Product
+from store.models import Order
+from store.models import OrderItem
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(
-        orderitem__product=F("pk")).distinct().order_by("title")
+    queryset = Order.objects.select_related("customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
 
-    return render(request, "hello.html", {"name": "Ikechukwu Agbarakwe", "products": queryset})
+    return render(request, "hello.html", {"name": "Ikechukwu Agbarakwe", "orders": queryset})
