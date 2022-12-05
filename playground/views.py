516cc47b74235f3935253b8b7f114de1c8a25f1e
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-12-05 18:18:30 +0100

    Working with Expression Wrappers

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func, Count
-from django.db.models.functions import Concat
-from store.models import Product, Customer
+from django.db.models import Value, F, ExpressionWrapper, DecimalField
+from store.models import Product
 
 
 def say_hello(request):
-    # result = Customer.objects.annotate(
-    #     full_name = Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')
-    # )
 
-    result = Customer.objects.annotate(
-        full_name=Concat('first_name', Value(
-            ' '), 'last_name'), orders_count=Count('order')
+    discounted_price = ExpressionWrapper(
+        F("unit_price") * 0.8, output_field=DecimalField())
+
+    result = Product.objects.annotate(
+        discounted_price=discounted_price
     )
 
     return render(request, "hello.html", {"name": "Ikechukwu Agbarakwe", "result": result})
