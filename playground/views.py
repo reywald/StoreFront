5cc6015cb6b504054c37ae9c356450476fea91b7
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-12-05 18:05:31 +0100

    Grouping Data

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from django.db.models import Value, F
-from store.models import Product, OrderItem
-
+from django.db.models import Value, F, Func, Count
+from django.db.models.functions import Concat
+from store.models import Product, Customer
 
 
 def say_hello(request):
-    result = OrderItem.objects.annotate(sub_total=F("unit_price") * F("quantity"))
+    # result = Customer.objects.annotate(
+    #     full_name = Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')
+    # )
+
+    result = Customer.objects.annotate(
+        full_name=Concat('first_name', Value(
+            ' '), 'last_name'), orders_count=Count('order')
+    )
+
     return render(request, "hello.html", {"name": "Ikechukwu Agbarakwe", "result": result})
