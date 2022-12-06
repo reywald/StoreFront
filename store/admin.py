080b0b3255d823dca7b829e03fb5833f2106cc74
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-12-06 16:43:36 +0100

    Overriding the Base QuerySet

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from django.db.models import Count
 from . import models
 
 
-admin.site.register(models.Collection)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product_count']
+
+    @admin.display(ordering="product_count")
+    def product_count(self, collection):
+        return collection.product_count
+
+    def get_queryset(self, request: HttpRequest):
+        return super().get_queryset(request).annotate(
+            product_count=Count("product")
+        )
 
 
 @admin.register(models.Product)
@@ -14,7 +27,7 @@ class ProductAdmin(admin.ModelAdmin):
     def collection_title(self, product):
         return product.collection.title
 
-    @admin.display(ordering='inventory')
+    @admin.display(ordering='inventory_status')
     def inventory_status(self, product):
         return "Low" if product.inventory < 10 else "OK"
 
