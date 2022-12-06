ace008c37eb0653b00c4c0b5c329917245e538c8
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-12-06 08:57:33 +0100

    Adding Computed Columns

--- store/admin.py
+++ store/admin.py
@@ -7,16 +7,18 @@ admin.site.register(models.Collection)
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
 
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        return "Low" if product.inventory < 10 else "OK"
 
+
+@admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
-
-
-admin.site.register(models.Customer, CustomerAdmin)
