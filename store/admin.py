ac41cf44ae6785bcb1fb13e4975dd39de582083f
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-12-14 09:15:05 +0100

    Adding Filtering to the List Page

--- store/admin.py
+++ store/admin.py
@@ -29,10 +29,25 @@ class CollectionAdmin(admin.ModelAdmin):
         )
 
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = "inventory"
+    parameter_name = "inventory"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("<10", "Low")
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "<10":
+            return queryset.filter(inventory__lt=10)
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status', 'collection']
     list_editable = ['unit_price']
+    list_filter = ["collection", "last_update", InventoryFilter]
     list_per_page = 10
 
     def collection_title(self, product):
